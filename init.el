788f2b19440cc9714c3d875dbc576f4a04396a64
Grant Horner <h.grant.horner@gmail.com>
2019-12-25 17:43:54 -0500

    added dark theme to init file

--- init.el
+++ init.el
@@ -28,6 +28,9 @@
   :config
   (exec-path-from-shell-initialize))
 
+(use-package gruber-darker-theme
+  :ensure t)
+
 ;; Sensible startup
 (setq inhibit-startup-screen t)
 (setq ring-bell-function 'ignore)
@@ -140,4 +143,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:foreground "#e4e4ef" :background "#181818")))))
