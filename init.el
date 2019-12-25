0bde25b24f3bfe677d4084e2d2cd0e8295cf53da
Grant Horner <h.grant.horner@gmail.com>
2019-12-25 17:24:06 -0500

    set up use package

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 ;; Set up Melpa
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -11,16 +14,20 @@
   )
 (package-initialize)
 
-;; Set lisp directory for additional startup stuff
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;; Initialize use package
+(eval-when-compile
+    (add-to-list 'load-path "/Users/grant/.emacs.d/elpa/use-package-20191126.2034")
+  (require 'use-package))
 
 ;; Set path
-(exec-path-from-shell-initialize)
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
 ;; Sensible startup
 (setq inhibit-startup-screen t)
 (setq ring-bell-function 'ignore)
-(set-frame-font "Ubuntu Mono 16" nil t)
+(set-frame-font "Ubuntu Mono 18" nil t)
 (setq backup-directory-alist '(("." . "~/.emacs_backups")))
 (setq mac-command-modifier 'control)
 (menu-bar-mode -1)
@@ -30,8 +37,13 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 (setq-default indent-tabs-mode nil)
-(whitespace-mode 1)
-(whitespace-newline-mode 0)
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")))
+
+;; Org mode
+(setq org-todo-keywords
+      '((sequence "TODO" "WAITING" "IN PROGRESS" "|" "DONE" "DELEGATED")))
 
 ;; Autocomplete brackets
 (electric-pair-mode 1)
@@ -39,28 +51,72 @@
 (setq electric-pair-preserve-balance nil)
 
 ;; Fix c indentation
-(require 'fix-c-indentation)
+(defun fix-c-indent-offset-according-to-syntax-context (key val)
+  "Fix c braces KEY VAL."
+  (setq c-offsets-alist (delq (assoc key c-offsets-alist) c-offsets-alist))
+  (add-to-list 'c-offsets-alist '(key . val)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              ;; indent
+              (fix-c-indent-offset-according-to-syntax-context 'substatement-open 0))
+            ))
 
 ;; Multiple cursors
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  ("C->" . 'mc/mark-next-like-this)
+  ("C-<" . 'mc/mark-previous-like-this)
+  ("C-c C-<" . 'mc/mark-all-like-this))
 
 ;; Turn on company mode
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package company
+  :ensure t
+  :diminish t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
 
-;; Haskell mode
-(require 'init-haskell)
+;; Flycheck config
+(use-package flycheck
+  :defer t
+  :ensure t
+  :hook (prog-mode . flycheck-mode)
+  :custom
+  (flycheck-emacs-lisp-load-path 'inherit)
+  :config
+  (flycheck-add-mode 'javascript-eslint 'js-mode)
+  (flycheck-add-mode 'typescript-tslint 'rjsx-mode))
 
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
+(use-package haskell-mode
+  :ensure t
+  :mode (("\\.hs\\'"    . haskell-mode)
+         ("\\.cabal\\'" . haskell-cabal-mode)
+         ("\\.hcr\\'"   . haskell-core-mode))
+  :interpreter ("haskell" . haskell-mode)
+  :init
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (add-hook 'haskell-mode-hook (lambda () (yas-minor-mode))))
+
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . 'magit-status))
 
 ;; Ivy
-(ivy-mode 1)
-(counsel-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(use-package ivy
+  :ensure t
+  :init
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  :config
+  (ivy-mode 1)
+  :bind ("C-x b" . 'ivy-switch-buffer))
+
+(use-package counsel
+  :ensure t
+  :config (counsel-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -71,9 +127,10 @@
  '(custom-safe-themes
    (quote
     ("47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" default)))
+ '(flycheck-emacs-lisp-load-path (quote inherit))
  '(package-selected-packages
    (quote
-    (multiple-cursors exec-path-from-shell flymake flymake-hlint haskell-mode magit company counsel ivy gruber-darker-theme smex)))
+    (use-package browse-kill-ring yasnippet-snippets yasnippet move-text multiple-cursors exec-path-from-shell flymake flymake-hlint haskell-mode magit company counsel ivy gruber-darker-theme smex)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
